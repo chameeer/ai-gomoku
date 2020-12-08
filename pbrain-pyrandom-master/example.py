c01355bb70c0611da448b5c9c5ef0201259bb25d
Yuer867 <62204893+Yuer867@users.noreply.github.com>
2020-12-08 15:30:19 +0800

    modify function pre_process(version2)

--- pbrain-pyrandom-master/example.py
+++ pbrain-pyrandom-master/example.py
@@ -267,16 +267,23 @@ def pre_process(board):
         board_copy_my = copy.deepcopy(board)
         board_copy_my[pos[0]][pos[1]] = 1
         type_list_my = be.board_type_count(board_copy_my, 'my', attack=False)
-
+        if type_list_my['FIVE'] != 0:
+            return pos[0], pos[1]
+        else:
+            continue
+    for pos in probOfPosition:
         board_copy_opponent = copy.deepcopy(board)
         board_copy_opponent[pos[0]][pos[1]] = 2
         type_list_opponent = be.board_type_count(board_copy_opponent, 'opponent', attack=False)
-
-        if type_list_my['FIVE'] != 0:
+        if type_list_opponent['FIVE'] != 0 or type_list_opponent['FOUR'] != 0:
             return pos[0], pos[1]
-        elif type_list_opponent['FIVE'] != 0 or type_list_opponent['FOUR'] != 0:
-            return pos[0], pos[1]
-        elif type_list_my['FOUR'] != 0:
+        else:
+            continue
+    for pos in probOfPosition:
+        board_copy_my = copy.deepcopy(board)
+        board_copy_my[pos[0]][pos[1]] = 1
+        type_list_my = be.board_type_count(board_copy_my, 'my', attack=False)
+        if type_list_my['FOUR'] != 0:
             return pos[0], pos[1]
         else:
             continue
